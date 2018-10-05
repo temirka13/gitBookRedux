2b5880568ff0fb3c2dd054004bba7582c123ffbe
temirka13 <temirka13@mail.ru>
2018-10-05 10:02:54 +0300

    обернули компоненты Page/User в отдельные контейнеры,чтобы избежать лишнего перерендинга

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,19 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
+import UserContainer from "../containers/UserContainer";
+import PageContainer from "../containers/PageContainer";
 import "../style/App.css";
-import { User } from "../components/User";
-import { Page } from "../components/Page";
-import { getPhotos } from "../actions/PageActions";
-import { handleLogin } from "../actions/UserActions";
 
 class App extends Component {
   render() {
-    const { user, page, getPhotos, handleLogin } = this.props;
     return (
       <div className="App">
-        <header className="App-header">
-          <User
-            name={user.name}
-            isFetching={user.isFetching}
-            error={user.error}
-            handleLogin={handleLogin}
-          />
-        </header>
-        <Page
-          photos={page.photos}
-          year={page.year}
-          loading={page.loading}
-          error={page.error}
-          getPhotos={getPhotos}
-        />
+        <div className="App-header">
+          <UserContainer />
+        </div>
+        <PageContainer />
       </div>
     );
   }
 }
 
-const mapStateToProps = store => {
-  console.log(store);
-  return {
-    user: store.user,
-    page: store.page
-  };
-};
-
-const mapDispatchToPtops = dispatch => {
-  return {
-    getPhotos: year => dispatch(getPhotos(year)),
-    handleLogin: () => dispatch(handleLogin())
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToPtops
-)(App);
+export default App;
